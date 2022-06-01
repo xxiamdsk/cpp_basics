446b7c31e507bb72a7866144629cc292bb04ed0d
Deepak Singh Kushwaha <95338176+xxiamdsk@users.noreply.github.com>
2022-06-01 12:04:14 +0530

    Uploaded 02_endl.cpp

---
+++ 01_basics_of_c++/02_endl.cpp
@@ -0,0 +1,20 @@
+// // Write a C++ program to print cpp on the first line and programming on the second line using endl.
+
+// // Header files
+#include <iostream>
+
+// // use namespace
+using namespace std;
+
+// // Main Function Start
+int main()
+{
+
+    cout << "cpp" << endl
+         << "programming"; // endl is used to add new line '\n' char
+
+    cout << endl; // Add new line
+    cin.ignore();
+    return 0;
+}
+// // Main Function End
