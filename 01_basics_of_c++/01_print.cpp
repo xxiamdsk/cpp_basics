3e6710ed38316144522d80bc8707470f8258daad
Deepak Singh Kushwaha <95338176+xxiamdsk@users.noreply.github.com>
2022-06-01 12:42:43 +0530

    Uploaded 01_print.cpp

---
+++ 01_basics_of_c++/01_print.cpp
@@ -0,0 +1,18 @@
+// // Write a C++ program to print "cpp-programming" on the screen.
+
+// // Header files
+#include <iostream>
+
+// // use namespace
+using namespace std;
+
+// // Main Function Start
+int main()
+{
+
+    cout << "cpp-programming"; // cout is a predefined object used to print output on screen
+
+    cin.ignore();
+    return 0;
+}
+// // Main Function End
